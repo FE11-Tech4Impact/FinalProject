abb1a1c855dbf6cb6991a54c4980712ca2269386
Hidayatul Nur Hanifah <120232865+hidayatulnh@users.noreply.github.com>
2023-06-10 08:49:36 +0700

    Update artikel.jsx

--- src/components/artikel.jsx
+++ src/components/artikel.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './artikel.css';
+import '../css/artikel.css';
 
 const Artikel = () => {
   const [articles, setArticles] = useState([]);
