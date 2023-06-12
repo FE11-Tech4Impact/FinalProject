bfda2c1d5a404efddd5c7eefdf4ac96b520cf759
dzulfilove <dzulfivector@gmail.com>
2023-06-12 03:43:09 -0700

    uy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,24 @@
-import { NavLink } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { HiShoppingCart, HiSearch, HiChevronDown } from "react-icons/hi";
+import { NavLink } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  HiShoppingCart,
+  HiSearch,
+  HiChevronDown,
+  HiMenu,
+} from "react-icons/hi";
 import React, { useState, useContext, useRef, useEffect } from "react";
-import '../css/header.css';
-import Logo from '../assets/logo.png';
-import '../globalstyle.css';
-
+import "../css/header.css";
+import Logo from "../assets/logo.png";
+import "../globalstyle.css";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   const loggedInUser = JSON.parse(localStorage.getItem("loggedInUser"));
   const [isOpen, setIsOpen] = useState(false);
   const [clickCount, setClickCount] = useState(0);
   const ref = useRef(null);
+  const [isSide, setIsSide] = useState(false);
 
   const toggleMenu = () => {
     setClickCount(clickCount + 1);
@@ -24,12 +30,23 @@ export const Header = () => {
       setIsOpen(true);
     }
   };
-  
+
+  const toggleSide = () => {
+    setClickCount(clickCount + 1);
+
+    if (clickCount === 1) {
+      setIsSide(false);
+      setClickCount(0);
+    } else {
+      setIsSide(true);
+    }
+  };
+
   const handleClickOutside = (event) => {
     if (ref.current && !ref.current.contains(event.target)) {
       setIsOpen(false);
       setClickCount(0);
-
+      setIsSide(false);
     }
   };
 
@@ -41,67 +58,133 @@ export const Header = () => {
     };
   }, []);
 
-
-  const exit =() => {
+  const exit = () => {
     // logout();
     localStorage.removeItem("loggedInUser");
     window.location.reload();
-  }
+  };
 
   return (
     <div>
       <div className="navbar">
         <div className="menubar">
           <img src={Logo} alt="Not Found" className="logo" />
-          <div className="menu">
+          <div className="menu men-nav">
             <NavLink exact to="/" className="nav-link" activeClassName="active">
               Beranda
             </NavLink>
-            <NavLink to="/cari-dokter" className="nav-link" activeClassName="active">
+            <NavLink
+              to="/cari-dokter"
+              className="nav-link"
+              activeClassName="active"
+            >
               Cari Dokter
             </NavLink>
-            <NavLink to="/artikel" className="nav-link" activeClassName="active">
+            <NavLink
+              to="/artikel"
+              className="nav-link"
+              activeClassName="active"
+            >
               Artikel
             </NavLink>
-            <NavLink to="/tokoObat" className="nav-link" activeClassName="active">
+            <NavLink
+              to="/tokoObat"
+              className="nav-link"
+              activeClassName="active"
+            >
               Toko Obat
             </NavLink>
+            <button onClick={exit} className="keluar">
+              Keluar
+            </button>
           </div>
         </div>
         <div className="icons">
           <HiSearch className="search-icon" />
           <HiShoppingCart className="cart-icon" />
           <div className="button-header">
-            {!loggedInUser ?(
+            {!loggedInUser ? (
               <>
-            <Button variant="outline-success">Daftar</Button>
-            <a href='/login' className="btn btn-success">Masuk</a> 
-            </>
-            )
-            : (
+                <Button variant="outline-success">Daftar</Button>
+                <Link to="/login" className="btn btn-success">
+                  Masuk
+                </Link>
+              </>
+            ) : (
               <>
-              <div className="info-title">
-              <div className='name'>
-                {loggedInUser.username.toString().charAt(0).toUpperCase()}
-              </div>
-              <h6 className="navbar-info-title">{loggedInUser.username.toString().charAt(0).toUpperCase() + loggedInUser.username.toString().slice(1)}</h6>
-              <div class="dropdown" ref={ref}>
-
-               <button onClick={toggleMenu}><HiChevronDown className='dropdown-button' /></button>
-               {isOpen && (
-                <div class="dropdown-content">
-                  <a href="#" className='dropdown-item'>Profil</a>
-                  <a href="#" className='dropdown-item'>Pengaturan</a>
-                  <button onClick={exit} className='dropdown-item'>Keluar</button>
+                <div className="info-title">
+                  <div className="name">
+                    {loggedInUser.username.toString().charAt(0).toUpperCase()}
+                  </div>
+                  <h6 className="navbar-info-title">
+                    {loggedInUser.username.toString().charAt(0).toUpperCase() +
+                      loggedInUser.username.toString().slice(1)}
+                  </h6>
+                  <div class="dropdown" ref={ref}>
+                    <button onClick={toggleMenu}>
+                      <HiChevronDown className="dropdown-button" />
+                    </button>
+                    {isOpen && (
+                      <div class="dropdown-content">
+                        <a href="#" className="dropdown-item">
+                          Profil
+                        </a>
+                        <a href="#" className="dropdown-item">
+                          Pengaturan
+                        </a>
+                        <button onClick={exit} className="dropdown-item">
+                          Keluar
+                        </button>
+                      </div>
+                    )}
+                  </div>
                 </div>
-               )}
-              </div>
-
-              </div>
               </>
             )}
           </div>
-       
+          <div class="dropdown-side" ref={ref}>
+            <button onClick={toggleSide} className="side">
+              <HiMenu className="burger-icon" />
+            </button>
+            {isSide && (
+              <>
+                <div className="menu men-side">
+                  <Link
+                    exact
+                    to="/"
+                    className="nav-link"
+                    activeClassName="active"
+                  >
+                    Beranda
+                  </Link>
+                  <Link
+                    to="/cari-dokter"
+                    className="nav-link"
+                    activeClassName="active"
+                  >
+                    Cari Dokter
+                  </Link>
+                  <Link
+                    to="/artikel"
+                    className="nav-link"
+                    activeClassName="active"
+                  >
+                    Artikel
+                  </Link>
+                  <Link
+                    to="/tokoObat"
+                    className="nav-link"
+                    activeClassName="active"
+                  >
+                    Toko Obat
+                  </Link>
+                  <button onClick={exit} className="keluar">
+                    Keluar
+                  </button>
+                </div>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </div>
